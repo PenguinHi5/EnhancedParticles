1f0b7d3c973b5a379bb71fda7c16037cb37299ab
Penguino <motpenguinhi5@gmail.com>
2021-12-19 03:42:06 -0700

    Updated pom.xml

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin
+{
+
+    public void onEnable()
+    {
+        
+    }
+
+    public void onDisable()
+    {
+
+    }
+
+}
